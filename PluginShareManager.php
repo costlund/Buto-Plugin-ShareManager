3eb07b378069e3ceb0df935b2d0002735b41064f
Claes Östlund <claes@stenbergit.se>
2025-06-02 13:12:14 +0200

    1.2.0

--- PluginShareManager.php
+++ PluginShareManager.php
@@ -6,6 +6,7 @@ class PluginShareManager{
     wfPlugin::enable('share/linkedin');
     wfPlugin::enable('share/mail_client');
     wfPlugin::enable('share/copy');
+    wfPlugin::enable('qr/link_to_page');
   }
   public function widget_share_buttons($data){
     $data = new PluginWfArray($data);
